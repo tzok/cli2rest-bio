28c450a0b702cf99bea320a77f0653b776194829
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-03-25 09:34:12 +0100

    style: Apply linter formatting to fr3d_runner.py

--- fr3d/fr3d_runner.py
+++ fr3d/fr3d_runner.py
@@ -12,15 +12,20 @@ from fr3d.classifiers.NA_pairwise_interactions import generatePairwiseAnnotation
 def process_cif(cif_path):
     # Get the directory containing the input file
     input_dir = os.path.dirname(cif_path)
-    base_name = os.path.basename(cif_path).split('.')[0]
-    
+    base_name = os.path.basename(cif_path).split(".")[0]
+
     # Create a temporary directory for output
     tmpdir = tempfile.mkdtemp()
 
     try:
         # Run FR3D analysis
         generatePairwiseAnnotation(
-            base_name, None, input_dir, tmpdir, "basepair_detail,stacking,backbone", "txt"
+            base_name,
+            None,
+            input_dir,
+            tmpdir,
+            "basepair_detail,stacking,backbone",
+            "txt",
         )
 
         # Read the results
@@ -43,10 +48,10 @@ def process_cif(cif_path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process CIF file with FR3D')
-    parser.add_argument('cif_file', help='Path to the CIF file to process')
+    parser = argparse.ArgumentParser(description="Process CIF file with FR3D")
+    parser.add_argument("cif_file", help="Path to the CIF file to process")
     args = parser.parse_args()
-    
+
     # Process the CIF file
     results = process_cif(args.cif_file)
 
