28747088c938bf4aa2f5ffd3df7407d8e13863f2
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-04-29 10:32:56 +0200

    feat: Use rsvg-convert to handle image conversion before cleaning

--- varna-tz/wrapper.sh
+++ varna-tz/wrapper.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 java -cp /varna-tz.jar pl.poznan.put.varna.AdvancedDrawer input.json
-svgcleaner output.svg clean.svg
+rsvg-convert --format svg --output converted.svg output.svg
+svgcleaner converted.svg clean.svg
